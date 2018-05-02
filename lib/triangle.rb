686512bc8e01ffd2de32b19ac36aa443922b8980
Marcus  = Soulo <souloinc87@gmail.com>
2018-05-02 18:27:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,19 +8,19 @@ class Triangle
   end
 
   def kind
-    if @side_a == 0 && @side_b == 0 && @side_c == 0
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
-    elsif @side_a == @side_b && @side_b == @side_c
+    if @side_a == @side_b && @side_b == @side_c
       :equilateral
     elsif @side_a < @side_b && @side_b == @side_c || @side_a > @side_b && @side_a == @side_c ||
       @side_a == @side_b && @side_b > @side_c
       :isosceles
     elsif @side_a != @side_b && @side_b != @side_c
       :scalene
+    elsif @side_a == 0 && @side_b == 0 && @side_c == 0
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
     end
   end
 
